751043643b6523b88490e15796062469b88b20db
Andy Vo <andy@voandy.com>
2017-05-31 06:58:46 +1000

    started work on key generation, added extended GCD

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # RSA Key Pair Generator and Encrypter/Decrypter
 
-A very basic (and hugely insecure) implementation of the RSA cryptosystem I wrote to get my feet wet in Haskell.
+A very basic (and massively insecure) implementation of the RSA cryptosystem I wrote to wet my feet in Haskell.
--- rsa.hs
+++ rsa.hs
@@ -1,3 +1,5 @@
+import System.Random
+
 -- naive primality test by trial division
 isPrime :: Integer -> Bool
 isPrime n
@@ -10,9 +12,27 @@ isPrime n
 
 -- returns the first prime greater than seed
 nextPrime :: Integer -> Integer
-nextPrime n = nextPrime' (n + 1)
-    where nextPrime' m = if isPrime m then m else nextPrime (n + 1)
+nextPrime n = if mod n 2 == 0 then nextPrime' (n + 1) else nextPrime' (n + 2)
+    where nextPrime' m = if isPrime m then m else nextPrime (n + 2)
 
 -- finds the greatest common divisor of m and n using the Euclidean algorithm
-gcd:: Integer -> Integer -> Integer
-gcd m n = if n == 0 then m else gcd n (mod m n)
+euclidGCD :: Integer -> Integer -> Integer
+euclidGCD m n = if n == 0 then m else euclidGCD n (mod m n)
+
+-- extended Euclidean algorithm
+-- wikibooks https://tinyurl.com/ybtrevtz
+extendedGCD :: Integer -> Integer -> (Integer, Integer, Integer)
+extendedGCD 0 b = (b, 0, 1)
+extendedGCD a b = let (g, s, t) = extendedGCD (b `mod` a) a
+    in (g, t - (b `div` a) * s, s)
+
+-- takes 3 seed values and returns a public/private key pair and their modulus
+generateKeys :: Integer -> Integer -> Integer -> (Integer, Integer, Integer)
+generateKeys min_p min_q min_e = (d, e, n)
+    where
+        p = nextPrime min_p
+        q = nextPrime min_q
+        e = nextPrime min_e
+        n = p * q
+        phi = (p - 1) * (q - 1)
+
