15738be615eaaee91308f26b5127525673c0c2f3
Andy Vo <andy@voandy.com>
2017-05-30 14:52:30 +1000

    completed nextPrime and gcd functions

--- rsa.hs
+++ rsa.hs
@@ -4,11 +4,15 @@ isPrime n
     | n <= 1 = False
     | n <= 3 = True
     | (mod n 2 == 0) || (mod n 3 == 0) = False
-    |    all ((>0).rem n) [5,11..floor.sqrt.fromIntegral $ n + 1]
-      && all ((>0).rem n) [7,13..floor.sqrt.fromIntegral $ n + 1] = True
-    | otherwise = False
+    | any ((==0).rem n) [5,11..floor.sqrt.fromIntegral $ n + 1] = False
+    | any ((==0).rem n) [7,13..floor.sqrt.fromIntegral $ n + 1] = False
+    | otherwise = True
 
 -- returns the first prime greater than seed
-brutePrime :: Integer -> Integer
-brutePrime seed
-brutePrime <= 2 = seed
+nextPrime :: Integer -> Integer
+nextPrime n = nextPrime' (n + 1)
+    where nextPrime' m = if isPrime m then m else nextPrime (n + 1)
+
+-- finds the greatest common divisor of m and n using the Euclidean algorithm
+gcd:: Integer -> Integer -> Integer
+gcd m n = if n == 0 then m else gcd n (mod m n)
