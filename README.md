31450bd275acb9bae1124e7ed6e5870eec43ee14
Andy Vo <andy@voandy.com>
2017-06-10 17:34:16 +1000

    cleaned up functions and made RSA.hs into module

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # RSA Key Pair Generator and Encrypter/Decrypter
 
-A very basic (and massively insecure) implementation of the RSA cryptosystem I wrote to wet my feet in Haskell.
+A very basic (and massively insecure) implementation of the RSA cryptosystem I wrote to wet my feet in Haskell. This code was written recreationally and it goes without saying it should not be used for real-world cryptography under and circumstances.
--- RSA.hs
+++ RSA.hs
@@ -1,8 +1,10 @@
-import System.Random
-import Data.Maybe
+module RSA
+( generateKeys
+, coPrime
+) where
 
 -- naive primality test by trial division
-isPrime :: Integer -> Bool
+isPrime :: (Integral a) => a -> Bool
 isPrime n
     | n <= 1 = False
     | n <= 3 = True
@@ -11,36 +13,39 @@ isPrime n
     | any ((==0).rem n) [7,13..floor.sqrt.fromIntegral $ n + 1] = False
     | otherwise = True
 
--- returns the first prime greater than a seed m
-nextPrime :: Integer -> Integer
+-- returns the first prime greater than the seed n
+nextPrime :: (Integral a) => a -> a
 nextPrime n = if mod n 2 == 0 then nextPrime' (n + 1) else nextPrime' n
     where nextPrime' m = if isPrime m then m else nextPrime (n + 2)
 
 -- finds the greatest common divisor of m and n using the Euclidean algorithm
-euclidGCD :: Integer -> Integer -> Integer
-euclidGCD m n = if n == 0 then m else euclidGCD n (mod m n)
+euclidGCD :: (Integral a) => a -> a -> a
+euclidGCD a b = if b == 0 then a else euclidGCD b (mod a b)
 
--- extended Euclidean algorithm https://rosettacode.org/wiki/Modular_inverse#Haskell
-extendedGCD :: Integer -> Integer -> (Integer, Integer, Integer)
+-- extended Euclidean algorithm 
+-- https://rosettacode.org/wiki/Modular_inverse
+extendedGCD :: (Integral a) => a -> a -> (a, a, a)
 extendedGCD a 0 = (1, 0, a)
 extendedGCD a b = let (q, r) = a `quotRem` b
                       (s, t, g) = extendedGCD b r in (t, s - q * t, g)
 
--- modular inverse https://rosettacode.org/wiki/Modular_inverse#Haskell
-modInv :: Integer -> Integer -> Maybe Integer
-modInv a m = let (i, _, g) = extendedGCD a m in if g == 1 then Just (mkPos i) else Nothing
+-- modular inverse, assumes a and m are coprime
+modInv :: (Integral a) => a -> a -> a
+modInv a m = let (i, _, _) = extendedGCD a m in mkPos i
     where mkPos x = if x < 0 then x + m else x
 
--- takes 3 seed values and returns a public/private key pair and their modulus
-generateKeys :: Integer -> Integer -> Integer -> (Integer, Integer, Integer)
-generateKeys min_p min_q min_e = (d, e, n)
-    where
-        p = nextPrime min_p
-        q = nextPrime min_q
-        n = p * q
-        phi = (p - 1) * (q - 1)
-        e = gcdFinder (nextPrime min_e) phi
-        d = fromJust (modInv e phi)
+-- increments e until it is coprime with phi
+coPrime :: (Integral a) => a -> a -> a
+coPrime e phi = if euclidGCD e phi == 1 then e else coPrime (nextPrime (e + 1)) phi
 
-gcdFinder :: Integer -> Integer -> Integer
-gcdFinder e phi = if euclidGCD e phi == 1 then e else gcdFinder (nextPrime (e + 1)) phi
+-- takes 3 seed values and returns a public/private key pair and their modulus
+generateKeys :: (Integral a) => a -> a -> a -> (a, a, a)
+generateKeys min_p min_q min_e = (d, e, n)
+    where p = nextPrime min_p
+          q = nextPrime min_q
+          phi = (p - 1) * (q - 1)
+          n = p * q
+          e = coPrime (nextPrime min_e) phi
+          d = modInv e phi
+
+-- strConvert :: (Integral a) => String -> [a]
